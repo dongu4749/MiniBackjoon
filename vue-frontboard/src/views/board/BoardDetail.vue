fed11c6e0776db883f1acb51f4f884088b84f331
dongu4749 <dongu4749@naver.com>
2022-12-08 21:01:57 +0900

    error_fix

--- vue-frontboard/src/views/board/BoardDetail.vue
+++ vue-frontboard/src/views/board/BoardDetail.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="board-detail">
     <div class="common-buttons">
-      <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnUpdate" v-show="(author === nowuser)">수정</button>&nbsp;
-           <button type="button" class="w3-button w3-round w3-red" v-on:click="fnDelete" v-show="(author === nowuser)">삭제</button>&nbsp;
+      <button type="button" class="w3-button w3-round w3-blue-gray" v-on:click="fnUpdate" >수정</button>&nbsp;
+           <button type="button" class="w3-button w3-round w3-red" v-on:click="fnDelete">삭제</button>&nbsp;
       <button type="button" class="w3-button w3-round w3-gray" v-on:click="fnList">목록</button>
     </div>
     <div class="board-contents">
